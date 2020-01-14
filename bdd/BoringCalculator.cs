8ccac67dccbade9a79b8985b7b043a3fdd1f798f
Krzysztof Dabrowski <01133318@pw.edu.pl>
2020-01-14 22:25:03 +0100

    Create boring calculator

---
+++ bdd/BoringCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bdd
+{
+    public class BoringCalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
