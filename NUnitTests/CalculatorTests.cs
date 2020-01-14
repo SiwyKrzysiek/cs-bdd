9946e08896ef52b5ad7eba1551de6a84fe0d468a
Krzysztof Dabrowski <01133318@pw.edu.pl>
2020-01-14 22:34:01 +0100

    Test calculator

---
+++ NUnitTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using bdd;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddTwoSmallNumbers()
+        {
+            //Given
+            var calculator = new BoringCalculator();
+            var a = 5.0;
+            var b = 10.0;
+
+            //When
+            var sum = calculator.Add(a, b);
+
+            //Then
+            Assert.AreEqual(15.0, sum);
+        }
+    }
+}
