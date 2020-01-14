3961de0206703d86fc1c37348289cfe5919c6586
Krzysztof Dabrowski <01133318@pw.edu.pl>
2020-01-14 23:19:19 +0100

    Create bdd test

---
+++ NUnitTests/CalculatorSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private readonly bdd.BoringCalculator calculator = new bdd.BoringCalculator();
+        private readonly List<double> _values = new List<double>();
+        private double _result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int value)
+        {
+            _values.Add(value);
+            Console.WriteLine(value);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Adding numbers!");
+            Assert.AreEqual(2, _values.Count);
+            _result = calculator.Add(_values[0], _values[1]);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _result);
+        }
+    }
+}
